5ca17603b2454155976a4fcfa48168d0d404807a
Du Shiqiao <lucidfrontier.45@gmail.com>
2026-05-29 22:51:34 +0900

    refactor(tests): convert class-based tests to functional style
    Remove class wrappers from unit tests, making each test a standalone
    function as per pytest functional style convention.

--- tests/test_lse.py
+++ tests/test_lse.py
@@ -4,83 +4,87 @@ import pytest
 from milgboost.objective.lse import lse
 
 
-class TestLSE:
-    def test_lse_basic(self) -> None:
-        y = np.array([1, 0, 1, 0])
-        bag_ids = np.array([0, 0, 1, 1])
-        preds = np.array([0.5, -0.5, 1.0, -1.0])
-        r = 1.0
+def test_lse_basic() -> None:
+    y = np.array([1, 0, 1, 0])
+    bag_ids = np.array([0, 0, 1, 1])
+    preds = np.array([0.5, -0.5, 1.0, -1.0])
+    r = 1.0
 
-        grad, hess = lse(y, bag_ids, preds, r)
+    grad, hess = lse(y, bag_ids, preds, r)
 
-        assert grad.shape == preds.shape
-        assert hess.shape == preds.shape
-        assert hess.min() >= 1e-4
+    assert grad.shape == preds.shape
+    assert hess.shape == preds.shape
+    assert hess.min() >= 1e-4
 
-    def test_lse_single_bag(self) -> None:
-        y = np.array([1, 0, 1])
-        bag_ids = np.array([0, 0, 0])
-        preds = np.array([1.0, 0.0, -1.0])
-        r = 1.0
 
-        grad, hess = lse(y, bag_ids, preds, r)
+def test_lse_single_bag() -> None:
+    y = np.array([1, 0, 1])
+    bag_ids = np.array([0, 0, 0])
+    preds = np.array([1.0, 0.0, -1.0])
+    r = 1.0
 
-        exp_preds = np.exp(preds)
-        bag_sum_exp = np.sum(exp_preds)
-        w_ij = exp_preds / bag_sum_exp
-        bag_p = 1.0 / (1.0 + np.exp(-np.log(bag_sum_exp)))
-        expected_grad = (bag_p - y) * w_ij
+    grad, hess = lse(y, bag_ids, preds, r)
 
-        assert grad.shape == preds.shape
-        assert hess.shape == preds.shape
-        assert np.allclose(grad, expected_grad)
+    exp_preds = np.exp(preds)
+    bag_sum_exp = np.sum(exp_preds)
+    w_ij = exp_preds / bag_sum_exp
+    bag_p = 1.0 / (1.0 + np.exp(-np.log(bag_sum_exp)))
+    expected_grad = (bag_p - y) * w_ij
 
-    def test_lse_binary_labels(self) -> None:
-        y = np.array([0, 1, 0, 1])
-        bag_ids = np.array([0, 0, 1, 1])
-        preds = np.array([0.0, 0.0, 0.0, 0.0])
-        r = 1.0
+    assert grad.shape == preds.shape
+    assert hess.shape == preds.shape
+    assert np.allclose(grad, expected_grad)
 
-        grad, hess = lse(y, bag_ids, preds, r)
 
-        assert grad.shape == preds.shape
-        assert hess.shape == preds.shape
-        assert np.all(hess > 0)
+def test_lse_binary_labels() -> None:
+    y = np.array([0, 1, 0, 1])
+    bag_ids = np.array([0, 0, 1, 1])
+    preds = np.array([0.0, 0.0, 0.0, 0.0])
+    r = 1.0
 
-    def test_lse_r_parameter_effect(self) -> None:
-        y = np.array([1, 0])
-        bag_ids = np.array([0, 0])
-        preds = np.array([1.0, -1.0])
+    grad, hess = lse(y, bag_ids, preds, r)
 
-        _, hess_r1 = lse(y, bag_ids, preds, r=1.0)
-        _, hess_r2 = lse(y, bag_ids, preds, r=2.0)
+    assert grad.shape == preds.shape
+    assert hess.shape == preds.shape
+    assert np.all(hess > 0)
 
-        assert not np.allclose(hess_r1, hess_r2)
 
-    def test_lse_hessian_positive(self) -> None:
-        y = np.array([1, 0, 1, 0, 1])
-        bag_ids = np.array([0, 0, 1, 1, 2])
-        preds = np.array([2.0, -2.0, 3.0, -3.0, 0.5])
-        r = 1.0
+def test_lse_r_parameter_effect() -> None:
+    y = np.array([1, 0])
+    bag_ids = np.array([0, 0])
+    preds = np.array([1.0, -1.0])
 
-        _, hess = lse(y, bag_ids, preds, r)
+    _, hess_r1 = lse(y, bag_ids, preds, r=1.0)
+    _, hess_r2 = lse(y, bag_ids, preds, r=2.0)
 
-        assert np.all(hess > 0)
+    assert not np.allclose(hess_r1, hess_r2)
 
-    def test_lse_known_values(self) -> None:
-        y = np.array([1.0, 0.0])
-        bag_ids = np.array([0, 0])
-        preds = np.array([0.0, 0.0])
-        r = 1.0
 
-        grad, hess = lse(y, bag_ids, preds, r)
+def test_lse_hessian_positive() -> None:
+    y = np.array([1, 0, 1, 0, 1])
+    bag_ids = np.array([0, 0, 1, 1, 2])
+    preds = np.array([2.0, -2.0, 3.0, -3.0, 0.5])
+    r = 1.0
 
-        bag_sum_exp = 2.0
-        bag_y = np.log(bag_sum_exp)
-        bag_p = 1.0 / (1.0 + np.exp(-bag_y))
-        w_ij = np.array([0.5, 0.5])
+    _, hess = lse(y, bag_ids, preds, r)
 
-        expected_grad = (bag_p - y) * w_ij
+    assert np.all(hess > 0)
 
-        assert np.allclose(grad, expected_grad)
-        assert hess.min() >= 1e-4
+
+def test_lse_known_values() -> None:
+    y = np.array([1.0, 0.0])
+    bag_ids = np.array([0, 0])
+    preds = np.array([0.0, 0.0])
+    r = 1.0
+
+    grad, hess = lse(y, bag_ids, preds, r)
+
+    bag_sum_exp = 2.0
+    bag_y = np.log(bag_sum_exp)
+    bag_p = 1.0 / (1.0 + np.exp(-bag_y))
+    w_ij = np.array([0.5, 0.5])
+
+    expected_grad = (bag_p - y) * w_ij
+
+    assert np.allclose(grad, expected_grad)
+    assert hess.min() >= 1e-4
